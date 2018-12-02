42064ab818a0f4fde120c5e56a7a8131739142ac
ofersarid <ofersarid@gmail.com>
2018-12-02 21:18:59 +0200

    wip

--- src/home/index.js
+++ src/home/index.js
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react';
 import cx from 'classnames';
-import {AccountCircle, Lock} from 'styled-icons/material'
+import { Phone } from 'styled-icons/material'
 import styles from './styles.scss';
 
 const Home = () => {
   return (
-    <Fragment>
-      <div className={styles.header}>
+    <Fragment >
+      <div className={styles.header} >
         <button
           className={cx('button', styles.clickCall)}
           onClick={() => document.location.href = 'tel:972509013034'} >
-          <Lock />
+          <Phone />
           050-901-3034
         </button >
-        <div className={styles.logo}/>
-      </div>
-      <div className={styles.movieContainer}>
+        <div className={styles.logo} />
+      </div >
+      <div className={styles.movieContainer} >
         <iframe
           src="https://www.youtube.com/embed/4cEkV0bP-Wo?autoplay=1&showinfo=0"
           frameBorder="0"
@@ -23,8 +23,8 @@ const Home = () => {
           allowFullScreen
           className={styles.movie}
         />
-      </div>
-    </Fragment>
+      </div >
+    </Fragment >
   )
 };
 
