bf03f1f590a791aa2cfc7dc2153bf47653c83787
ofersarid <ofersarid@gmail.com>
2019-01-24 22:26:49 +0200

    wip

---
+++ src/analytics/index.js
@@ -0,0 +1,20 @@
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-132363213-1');
+
+const PRODUCTION = window.location.hostname === 'omerkadmi.co.il';
+// const PRODUCTION = true;
+
+export const trackPageView = () => {
+  if (!PRODUCTION) return;
+  ReactGA.pageview(window.location.hash);
+};
+
+export const trackClick = (category, action, label) => {
+  if (!PRODUCTION) return;
+  ReactGA.event({
+    category,
+    action,
+    label,
+  });
+};
--- src/website/app/components/nav-bar/nav-bar.js
+++ src/website/app/components/nav-bar/nav-bar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import cx from 'classnames';
 import Device from '/src/cms/device';
 import styles from './styles.scss';
 import { Call } from 'styled-icons/material/Call';
 import { Button } from '/src/cms/elements';
+import { trackClick } from '/src/analytics';
 import { navBar } from '../../types';
 import logo from './logo.png';
 
-const NavBar = () => (
+const NavBar = props => (
   <div className={styles.navBar} >
     <img src={logo} />
     <div >
       <Button
         className={styles.btn}
+        onClick={() => {
+          document.location.href = 'tel:972509013034';
+          trackClick('user', 'click', 'call-button');
+        }}
       >
-        <Call className={styles.phone}/>
-        <div>050-901-3034</div>
+        <Call className={cx(styles.phone, styles[`phone-${props.deviceType}`])} />
+        <div className={cx(styles[`number-${props.deviceType}`])}>050-901-3034</div >
       </Button >
     </div >
   </div >
