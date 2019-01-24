8cbabc36abdbf41ab8367fadf9bde66eaca079ef
ofersarid <ofersarid@gmail.com>
2019-01-24 22:12:44 +0200

    wip

--- src/website/app/components/nav-bar/nav-bar.js
+++ src/website/app/components/nav-bar/nav-bar.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Device from '/src/cms/device';
 import styles from './styles.scss';
+import { Call } from 'styled-icons/material/Call';
+import { Button } from '/src/cms/elements';
 import { navBar } from '../../types';
 import logo from './logo.png';
 
 const NavBar = () => (
   <div className={styles.navBar} >
     <img src={logo} />
+    <div >
+      <Button
+        className={styles.btn}
+      >
+        <Call className={styles.phone}/>
+        <div>050-901-3034</div>
+      </Button >
+    </div >
   </div >
 );
 
